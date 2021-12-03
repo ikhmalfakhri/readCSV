815237bae2deef1a1cc60b36d3fcfd003d0fcba0
Faris-Faiz <u2000421@siswa.um.edu.my>
2021-12-03 15:16:44 +0800

    added functionality to choose your course, and proper commenting and the likes

--- ReadCSVFile/src/readcsvfile/ReadCSVFile.java
+++ ReadCSVFile/src/readcsvfile/ReadCSVFile.java
@@ -4,10 +4,9 @@
  */
 package readcsvfile;
 
+import java.awt.HeadlessException;
 import java.io.*;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -20,40 +19,108 @@ public class ReadCSVFile {
 
     public static void main(String[] args) {
         String filepath = "src\\Book1.csv";
-        String module;
-        module = JOptionPane.showInputDialog(null, "Which module? (CASE SENSITIVE!)\n ", "Pick your module!", JOptionPane.INFORMATION_MESSAGE);
-        readRecord(module, filepath);
+        String[] courses = {
+            "Backelor of Computer Science (Computer Systems and Networking)",
+            "Backelor of Computer Science (Artificial Intelligence)",
+            "Backelor of Computer Science (Information Systems)",
+            "Backelor of Computer Science (Software Engineering)",
+            "Backelor of Information Technology (Multimedia)",
+            "Backelor of Information Technology (Data Science)"
+        }; //storage of all courses
+        String module, course;
+        int checker = 1;
+        course = (String) JOptionPane.showInputDialog(
+                null,
+                "Which module? (CASE SENSITIVE!)\n ",
+                "Pick your module!",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                courses,
+                courses[0]);
+
+        int courseSimple = simplifiedCourse(course); //to convert the chosen course of study into a simpler form. This is because IT students have  WIB2001 whereas CS students have WIA2001, but theyre the same subject.
+
+        while(checker!=0){ //so that the user doesn't have to always run the program again
+            if (courseSimple == 1) { //for CS Students
+            String moduleInput = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Which module? (CASE SENSITIVE!)\nNOTE: YOU ARE A COMPUTER SCIENCE STUDENT, YOUR COURSE IS WIA2001, THE REST ARE THE SAME",
+                    "Inputting course code",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+            module = moduleInput.toUpperCase(); //to ensure everything the user inputs is capitalized
+            if(module.equals("WIB2001")){ //to ensure the student wrote the correct course based on their respective departments.
+                JOptionPane.showMessageDialog(null, "I lit'rally told you to not put that.");
+            } else{ //if they typed correctly, the checker becomes 0, and it proceeds to show the course code and details.
+                checker = 0;
+                readRecord(module, filepath); //display the course details
+            }
+        } else { //for IT Students
+            module = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Which module? (CASE SENSITIVE!)\nNOTE: YOU ARE AN INFORMATION TECHNOLOGY STUDENT, YOUR COURSE IS WIB2001, THE REST ARE THE SAME",
+                    "Inputting course code",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+            if(module.equals("WIA2001")){
+                JOptionPane.showMessageDialog(null, "I lit'rally told you to not put that.");
+            } else{
+                checker = 0;
+                readRecord(module, filepath); //display the course details
+            }
+        }
+        }
+        
+
     }
 
-    public static void readRecord(String module, String filePath) {
-        boolean found = false;
+    public static void readRecord(String module, String filePath) { //to read the csv file
+        boolean found = false; //to check if the desired keyword is found within the csv file
         String moduleCode = "";
         String creditHour = "";
         String noOcc = "";
         String activityType = "";
         try {
-            x = new Scanner(new File(filePath));
-            x.useDelimiter("[,\n]");
+            x = new Scanner(new File(filePath)); //the file path is parsed into the Scanner constructor, meaining the Scanner class is scanning the file specified in the filepath, in this case, the CSV file
+            x.useDelimiter("[,\n]"); //to distinguish between each element in the csv. \n is so that at the end of the column, it sees a newline, so it knows its the end of the column already
 
-            while (x.hasNext() && !found) {
-                moduleCode = x.next();
-                creditHour = x.next();
-                noOcc = x.next();
-                activityType = x.next();
+            while (x.hasNext() && !found) { //.hasNext() means there's something existing after the element being scanned at the moment. !found means as long as its not found yet lah
+                moduleCode = x.next(); //reads the first column of CSV file and stores in moduleCode.
+                creditHour = x.next(); //reads the second column of CSV file and stores in creditHour.
+                noOcc = x.next(); //reads the third column of CSV file and stores in noOcc
+                activityType = x.next(); //reads the contents of the fourth column of the CSV file and stores in the activityType variable
 
-                if (moduleCode.equals(module)) {
+                if (moduleCode.equals(module)) { //to see if the desired module is found in the csv file in any of the rows
                     found = true;
                 }
             }
 
             if (found) {
-                JOptionPane.showMessageDialog(null, "Module Code: " + moduleCode + "\n Credits: " + creditHour + "\n No. of Occ: " + noOcc + "\n Activity Type: " + activityType);
+                JOptionPane.showMessageDialog(null, "Module Code: " + moduleCode + "\nCredits: " + creditHour + "\nNo. of Occ: " + noOcc + "\nActivity Type: " + activityType);
             } else {
                 JOptionPane.showMessageDialog(null, "Module does not exist.");
             }
-        } catch (Exception e) {
 
+        } catch (HeadlessException | FileNotFoundException e) {
+            System.out.println(e);
         }
+    }
+
+    public static int simplifiedCourse(String course) { //to simplify distinguishability between CS students and IT students. 1 for CS students, 0 for IT students.
+
+        int simplifiedCourse = switch (course) {
+            case "Backelor of Computer Science (Computer Systems and Networking)" ->
+                1;
+            case "Backelor of Computer Science (Artificial Intelligence)" ->
+                1;
+            case "Backelor of Computer Science (Information Systems)" ->
+                1;
+            case "Backelor of Computer Science (Software Engineering)" ->
+                1;
+            default ->
+                0;
+        };
+        return simplifiedCourse;
     }
 
 }
